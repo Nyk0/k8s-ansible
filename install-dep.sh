3a542452fa736195daa0115b11563b30270092c4
root <root@gateway.lab.local>
2021-05-13 16:10:13 +0200

    delete binary

--- install-dep.sh
+++ install-dep.sh
@@ -5,3 +5,7 @@ apt-get update
 apt-get install -y python3-virtualenv python3-venv python3-pip
 update-alternatives --install /usr/bin/python python /usr/bin/python3.7 3
 
+python -m venv venv
+source ./venv/bin/activate
+pip install --upgrade pip
+python -m pip install -r requirements.txt
